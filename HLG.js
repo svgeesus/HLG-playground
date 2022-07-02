81d96cb21b2cd542d24fd44abbf61b9230227221
Chris Lilley <chris@w3.org>
2022-07-02 14:27:17 +0300

    A bit more work, not finished though

--- HLG.js
+++ HLG.js
@@ -1,4 +1,4 @@
-// ITU-R BT.2390-8 p.26
+// ITU-R BT.2390-10 p.23
 // 6.1 The hybrid log-gamma opto-electronic transfer function (OETF)
 
 function HLG_OETF (E) {
@@ -13,7 +13,7 @@ function HLG_OETF (E) {
     }
 }
 
-// ITU-R BT.2390-8 p.33
+// ITU-R BT.2390-10 p.30 section
 // 6.3 The hybrid log-gamma electro-optical transfer function (EOTF)
 function HLG_inv_OETF (Edash) {
     const a = 0.17883277;
@@ -43,7 +43,7 @@ function fromHLG (RGB) {
 // Lw, the nominal peak luminance of the display in cd/m2
 // and Lb the display luminance for black in cd/m2,
 // return β the black level lift
-// ITU-R BT.2390-8 section
+// ITU-R BT.2390-10 p.30 section
 // 6.3 The hybrid log-gamma electro-optical transfer function (EOTF)
 // Default gamma is 1.2
 // Default white, black values are from VESA DisplayHDR 1.1, Performance Tier 1000
@@ -54,9 +54,9 @@ function blackLevelLift (gamma=1.2, Lw=1000, Lb=0.05) {
 // given Lw, the nominal peak luminance of the display in cd/m2
 // return the OOTF reference gamma (for reference viewing environments)
 // using the extended model
-// ITU-R BT.2390-8 p.31 section
+// ITU-R BT.2390-10 p.26 section
 // 6.2 System gamma and the opto-optical transfer function (OOTF)
-function extendedGamma (Lw) {
+function extendedGamma (Lw=1000) {
     const k = 1.111;
     let pow = Math.log2(Lw/1000);
     return 1.2 * Math.pow(k, pow);
@@ -67,7 +67,7 @@ function extendedGamma (Lw) {
 // return an adjusted system gamma for that environment
 // using the "best fit" model
 // because the "alternative model" is underspecified :(
-// ITU-R BT.2390-8 p.31 section
+// ITU-R BT.2390-10 p.26 section
 // 6.2 System gamma and the opto-optical transfer function (OOTF)
 function brightGamma (refGamma, Lamb) {
     let adjustment = -0.076 * Math.log10 (Lamb / 5);
@@ -78,18 +78,29 @@ function brightGamma (refGamma, Lamb) {
 // and an overall system gamma
 // apply that gamma to just the luminance
 // and return an array of adjusted linear-light RGB values
-// ITU-R BT.2390-8 p.29 section
+// ITU-R BT.2390-10 p.26 section
 // 6.2 System gamma and the opto-optical transfer function (OOTF)
 function HLG_OOTF(RGB, gamma) {
-    let [R, G, B] = RGB;
-    let Y = 0.2627 * R + 0.6780 * G + 0.0593 * B;
+    const toXYZ_M = [
+        [ 0.6369580483012914, 0.14461690358620832,  0.1688809751641721  ],
+        [ 0.2627002120112671, 0.6779980715188708,   0.05930171646986196 ],
+        [ 0.000000000000000,  0.028072693049087428, 1.060985057710791   ]
+    ];
+    const fromXYZ_M = [
+        [  1.716651187971268,  -0.355670783776392, -0.253366281373660  ],
+        [ -0.666684351832489,   1.616481236634939,  0.0157685458139111 ],
+        [  0.017639857445311,  -0.042770613257809,  0.942103121235474  ]
+    ]
+    let [X, Y, Z] = multiplyMatrices(toXYZ_M, RGB);
+    Y = Y ** gamma;
+    return multiplyMatrices(fromXYZ_M, [X, Y, Z]);
 }
 
 // given RGB, an array of HLG-encoded rec2100 RGB values in nominal range [0-1]
 // and the black level lift needed for a given display,
 // and the OOTF gamma needed for a given display peak luminance and ambient lightness,
 // return an array of  linear-light rec2100 RGB values in clamped range [0,1]
-// ITU-R BT.2390-8 section
+// ITU-R BT.2390-10 p.30 section
 // 6.3 The hybrid log-gamma electro-optical transfer function (EOTF)
 function HLG_EOTF (RGB, beta, gamma) {
 
@@ -103,17 +114,18 @@ function HLG_EOTF (RGB, beta, gamma) {
     return HLG_OOTF(RGB2, gamma);
 }
 
-function HLG_inv_EOTF () {
-    // umm
-}
 
 // convert array of sRGB (or rec709 full-range) in nominal range [0,1]
 // to rec2100-hlg
 // adapted from https://lists.w3.org/Archives/Public/public-colorweb/2021Sep/0008.html
 // uses conversion functions from CSS Color 4
-function convertExtendedSRGBtoREC2100HLG(sRGB) {
-    // linear-light value of HLG media white = 75IRE
-    const SrgbtoHlgScaler = HLG_inv_OETF(0.75);
+
+// linear-light value of HLG media white = 75IRE
+const SrgbtoHlgScaler = HLG_inv_OETF(0.75);
+
+
+function convertExtendedSRGBtoREC2100HLG (sRGB) {
+
 
     // undo sRGB transfer function to yeild linear-light RGB
     let linsRGB = lin_sRGB(sRGB);
@@ -136,3 +148,14 @@ function convertExtendedSRGBtoREC2100HLG(sRGB) {
     // (r4, g4, b4) = tf.hlg_inverse_eotf(r3, g3, b3, systemGamma)  # Convert to HLG Non-linear Signal (straightforward as systemGamma = 1.0)
 
 }
+
+// given HLG-encoded BT.2100 values, and also:
+// Lw, the display monitor peak luminance
+// Lb, the display monitor black luminance, and
+// Lamb, the ambient luminance
+// return the XYZ values for a display
+function complete_REC2100HLGtoXYZ (sRGB, Lw=1000, Lb=0.05, Lamb=0.05, OOTF_gama=1.2 {
+
+    // calculate black lift
+    let beta = blackLevelLift()
+})
